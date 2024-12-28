95632709417d13360d47a5b680a30e1328b8ce84
Daniel Thayer <DanielBlackThayer@gmail.com>
2024-12-27 21:37:29 -0800

    Test Client upload for showing how to use WinUI interfaces to get the information about a PLC variable and its value

---
+++ Test Client/App.xaml.cs
@@ -0,0 +1,76 @@
+﻿// Ignore Spelling: App
+
+using Microsoft.UI.Xaml;
+using System;
+
+namespace Test_Client
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public partial class App : Application, IDisposable
+    {
+        #region Members
+        /// <summary>
+        /// Main window for the application
+        /// </summary>
+        private MainWindow? m_window;
+        /// <summary>
+        /// Dispose flag
+        /// </summary>
+        private bool disposedValue;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Application Overrides
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            m_window.Activate();
+        }
+        #endregion
+
+        #region IDisposable Support
+        /// <summary>
+        /// Dispose of the object
+        /// </summary>
+        /// <param name="disposing">Dispose of managed objects</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    m_window?.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        /// <summary>
+        /// Dispose of the object
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
---
+++ Test Client/MainWindow.xaml.cs
@@ -0,0 +1,302 @@
+using AdsSimplifiedInterface;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TextBox;
+
+namespace Test_Client
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window, IDisposable
+    {
+        #region Non-Visible Members
+        /// <summary>
+        /// PLC Connection
+        /// </summary>
+        private readonly AdsInterface adsInterface;
+        /// <summary>
+        /// Dispose flag
+        /// </summary>
+        private bool disposedValue;
+        #endregion
+
+        #region Constructor
+        /// <summary>
+        /// Main constructor for the window
+        /// </summary>
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Build configuration and logging interfaces
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile($"appsettings.json", true, true).Build();
+            ILoggerFactory loggerFactory = LoggerFactory.Create(configure =>
+            {
+                configure.AddConsole();
+                configure.SetMinimumLevel(LogLevel.Information);
+            });
+
+            // Create the ADS connection
+            adsInterface = new(config, loggerFactory.CreateLogger<AdsInterface>());
+
+            // Get a list of the PLC variables
+            List<string> variables = adsInterface.GetVariables();
+
+            // Add the variables to the list view
+            foreach (string variable in variables)
+            {
+                CreateNode(variable);
+            }
+
+            tvVariables.UpdateLayout();
+        }
+        #endregion
+
+        #region Window Events
+        /// <summary>
+        /// Event for when a variable is tapped
+        /// </summary>
+        /// <param name="sender">Object selecting the variable</param>
+        /// <param name="args">Parameters of the selection</param>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "Required by API")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "Bug in VS doesn't see usage in MainWindow.xaml")]
+        private void TvVariables_Tapped(object sender, Microsoft.UI.Xaml.Input.TappedRoutedEventArgs e)
+        {
+            if (tvVariables.SelectedItem != null)
+            {
+                try
+                {
+                    string path = BuildPath((TreeViewNode)tvVariables.SelectedItem);
+                    PlcVariableTypeInfo? variable = adsInterface.GetVariableInfos(path).FirstOrDefault();
+
+                    if (variable != null)
+                    {
+                        tbPath.Text = path;
+                        tbType.Text = variable.DataType;
+                        tbSize.Text = variable.Size.ToString();
+
+                        object? value = adsInterface.GetValue(path);
+                        if (value == null)
+                        {
+                            tbValue.Text = "";
+                        }
+                        else if (variable.IsEnum)
+                        {
+                            tbValue.Text = variable.Children.FirstOrDefault(ev => ev.EnumValue.Equals(value))?.Name ?? value.ToString();
+                        }
+                        else if (value.GetType().IsPrimitive)
+                        {
+                            tbValue.Text = value.ToString();
+                        }
+                        else
+                        {
+                            tbValue.Text = JsonConvert.SerializeObject(value, Formatting.Indented);
+                        }
+                    }
+                }
+                catch
+                {
+                    tbPath.Text = string.Empty;
+                    tbType.Text = string.Empty;
+                    tbSize.Text = string.Empty;
+                    tbValue.Text = string.Empty;
+                }
+            }
+        }
+        #endregion
+
+        #region Helper Functions
+        /// <summary>
+        /// Create a node in the tree view
+        /// </summary>
+        /// <param name="name">Name of the node</param>
+        private void CreateNode(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+            else if (name.Contains('.') && !name[..name.IndexOf('.')].Contains('['))
+            {
+                string parent = name[..name.IndexOf('.')];
+                string child = name[(name.IndexOf('.') + 1)..];
+
+                TreeViewNode? parentNode = tvVariables.RootNodes.FirstOrDefault(node => ((string)node.Content).Equals(parent, StringComparison.OrdinalIgnoreCase));
+                if (parentNode == null)
+                {
+                    parentNode = new TreeViewNode() { Content = parent };
+                    tvVariables.RootNodes.Add(parentNode);
+                }
+
+                CreateNode(child, parentNode);
+            }
+            else if (name.StartsWith('['))
+            {
+                string self = name[..(name.IndexOf(']') + 1)];
+                string child = (name.IndexOf(']') + 2) <= name.Length ? name[(name.IndexOf(']') + 2)..] : string.Empty;
+
+                TreeViewNode? parentNode = tvVariables.RootNodes.FirstOrDefault(node => ((string)node.Content).Equals(self, StringComparison.OrdinalIgnoreCase));
+                if (parentNode == null)
+                {
+                    parentNode = new TreeViewNode() { Content = self };
+                    tvVariables.RootNodes.Add(parentNode);
+                }
+
+                CreateNode(child, parentNode);
+            }
+            else if (name.Contains('['))
+            {
+                string parent = name[..name.IndexOf('[')];
+                string child = name[name.IndexOf('[')..];
+
+                TreeViewNode? parentNode = tvVariables.RootNodes.FirstOrDefault(node => ((string)node.Content).Equals(parent, StringComparison.OrdinalIgnoreCase));
+                if (parentNode == null)
+                {
+                    parentNode = new TreeViewNode() { Content = parent };
+                    tvVariables.RootNodes.Add(parentNode);
+                }
+
+                CreateNode(child, parentNode);
+            }
+            else
+            {
+                TreeViewNode? self = tvVariables.RootNodes.FirstOrDefault(node => ((string)node.Content).Equals(name, StringComparison.OrdinalIgnoreCase));
+                if (self == null)
+                {
+                    self = new TreeViewNode() { Content = name };
+                    tvVariables.RootNodes.Add(self);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Create a node in the tree view
+        /// </summary>
+        /// <param name="name">Name of the node</param>
+        /// <param name="parent">Parent node</param>
+        private void CreateNode(string name, TreeViewNode parent)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return;
+            }
+            else if (name.Contains('.') && !name[..name.IndexOf('.')].Contains('['))
+            {
+                string self = name[..name.IndexOf('.')];
+                string child = name[(name.IndexOf('.') + 1)..];
+
+                TreeViewNode? parentNode = parent.Children.FirstOrDefault(node => ((string)node.Content).Equals(self, StringComparison.OrdinalIgnoreCase));
+                if (parentNode == null)
+                {
+                    parentNode = new TreeViewNode() { Content = self };
+                    parent.Children.Add(parentNode);
+                }
+
+                CreateNode(child, parentNode);
+            }
+            else if (name.StartsWith('['))
+            {
+                string self = name[..(name.IndexOf(']') + 1)];
+                string child = (name.IndexOf(']') + 2) <= name.Length ? name[(name.IndexOf(']') + 2)..] : string.Empty;
+
+                TreeViewNode? parentNode = parent.Children.FirstOrDefault(node => ((string)node.Content).Equals(self, StringComparison.OrdinalIgnoreCase));
+                if (parentNode == null)
+                {
+                    parentNode = new TreeViewNode() { Content = self };
+                    parent.Children.Add(parentNode);
+                }
+
+                CreateNode(child, parentNode);
+            }
+            else if (name.Contains('['))
+            {
+                string self = name[..name.IndexOf('[')];
+                string child = name[name.IndexOf('[')..];
+
+                TreeViewNode? parentNode = parent.Children.FirstOrDefault(node => ((string)node.Content).Equals(self, StringComparison.OrdinalIgnoreCase));
+                if (parentNode == null)
+                {
+                    parentNode = new TreeViewNode() { Content = self };
+                    parent.Children.Add(parentNode);
+                }
+
+                CreateNode(child, parentNode);
+            }
+            else
+            {
+                TreeViewNode? self = tvVariables.RootNodes.FirstOrDefault(node => ((string)node.Content).Equals(name, StringComparison.OrdinalIgnoreCase));
+                if (self == null)
+                {
+                    self = new TreeViewNode() { Content = name };
+                    parent.Children.Add(self);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Build the path to the variable
+        /// </summary>
+        /// <param name="node">Node for the variable</param>
+        /// <returns>Path to the variable</returns>
+        private static string BuildPath(TreeViewNode node)
+        {
+            string path = (string)node.Content;
+            while (node.Parent != null)
+            {
+                node = node.Parent;
+
+                if (node.Content == null)
+                {
+                    continue;
+                }
+
+                if (path.StartsWith('['))
+                {
+                    path = (string)node.Content + path;
+                }
+                else
+                {
+                    path = (string)node.Content + "." + path;
+                }
+            }
+            return path;
+        }
+        #endregion
+
+        #region IDisposable Support
+        /// <summary>
+        /// Dispose of the object
+        /// </summary>
+        /// <param name="disposing">Dispose managed objects</param>
+        private void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    adsInterface.Dispose();
+                }
+
+                disposedValue = true;
+            }
+        }
+
+        /// <summary>
+        /// Dispose of the object
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
